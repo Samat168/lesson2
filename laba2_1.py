2a4f1d34f261301709bc27429c2809e570986e64
Samat168 <supersamat2004@gmail.com>
2024-09-11 11:15:21 +0600

    e

--- laba2_1.py
+++ laba2_1.py
@@ -1,8 +1,8 @@
-sent = input("Введите строку: ")  
-words = sent.split(';')   
-longest = words[0]   
-for i in words: 
-    if len(i) > len(longest):  
-        longest = i   
+a = input("Введите строку: ")  
+b = a.split(';')   
+c = b[0]   
+for i in b: 
+    if len(i) > len(c):  
+        c = i   
  
-print("Самое длинное слово:", longest)
+print("Самое длинное слово:", c)
